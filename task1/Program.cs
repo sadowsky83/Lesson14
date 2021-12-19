f5ec72c6cee5e5e92dd543f10fa1af683326c3f5
Anatoly Sadowsky <sadowsky83@gmail.com>
2021-12-19 21:25:14 +0700

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        // Неведома зверушка
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Кот", "Мяу!");
+            Dog dog = new Dog("Пёс", "Гав!");
+            cat.ShowInfo();
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        string say;
+
+        public Animal(string name = "Хомяк", string say = "Му!")
+        {
+            this.Name = name;
+            this.say = say;          
+        }
+
+        public abstract void Say();        
+
+        public void ShowInfo()
+        {           
+            Console.WriteLine("Зверушка называется " + Name);
+            Say();
+        }
+    } 
+    class Cat : Animal
+    {
+        private string name;
+        private string say;
+        public override string Name { 
+            get => name;
+            set => name = value; 
+        }
+
+        public Cat(string name, string say)
+            : base(name)
+        {
+            this.say = say;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine($"{name} говорит {say}");
+        }
+    }
+
+    class Dog : Animal
+    {
+        private string name;
+        private string say;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Dog(string name, string say)
+            : base(name)
+        {
+            this.say = say;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine($"{name} говорит {say}");
+        }
+    }
+
+}
